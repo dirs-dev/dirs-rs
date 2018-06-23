dea79e9f087cf8a44c1d553adbcf8ede10f3a1b6
Simon Ochsenreither <simon@ochsenreither.de>
2018-06-23 13:27:09 +0200

    Move `home_dir` from `PathBuf` to `Option<PathBuf>`

--- src/win.rs
+++ src/win.rs
@@ -10,33 +10,37 @@ use self::winapi::um::shlobj;
 use self::winapi::um::shtypes;
 use self::winapi::um::winnt;
 
-pub fn home_dir()       -> PathBuf { unsafe { known_folder(&knownfolders::FOLDERID_Profile) } }
-pub fn data_dir()       -> PathBuf { unsafe { known_folder(&knownfolders::FOLDERID_RoamingAppData) } }
-pub fn data_local_dir() -> PathBuf { unsafe { known_folder(&knownfolders::FOLDERID_LocalAppData) } }
-pub fn cache_dir()      -> PathBuf { data_local_dir() }
-pub fn config_dir()     -> PathBuf { data_dir() }
+pub fn home_dir()       -> Option<PathBuf> { unsafe { known_folder(&knownfolders::FOLDERID_Profile) } }
+pub fn data_dir()       -> Option<PathBuf> { unsafe { known_folder(&knownfolders::FOLDERID_RoamingAppData) } }
+pub fn data_local_dir() -> Option<PathBuf> { unsafe { known_folder(&knownfolders::FOLDERID_LocalAppData) } }
+pub fn cache_dir()      -> Option<PathBuf> { data_local_dir() }
+pub fn config_dir()     -> Option<PathBuf> { data_dir() }
 pub fn executable_dir() -> Option<PathBuf> { None }
 pub fn runtime_dir()    -> Option<PathBuf> { None }
-pub fn audio_dir()      -> Option<PathBuf> { Some(unsafe { known_folder(&knownfolders::FOLDERID_Music) }) }
-pub fn desktop_dir()    -> Option<PathBuf> { Some(unsafe { known_folder(&knownfolders::FOLDERID_Desktop) }) }
-pub fn document_dir()   -> Option<PathBuf> { Some(unsafe { known_folder(&knownfolders::FOLDERID_Documents) }) }
-pub fn download_dir()   -> Option<PathBuf> { Some(unsafe { known_folder(&knownfolders::FOLDERID_Downloads) }) }
+pub fn audio_dir()      -> Option<PathBuf> { unsafe { known_folder(&knownfolders::FOLDERID_Music) } }
+pub fn desktop_dir()    -> Option<PathBuf> { unsafe { known_folder(&knownfolders::FOLDERID_Desktop) } }
+pub fn document_dir()   -> Option<PathBuf> { unsafe { known_folder(&knownfolders::FOLDERID_Documents) } }
+pub fn download_dir()   -> Option<PathBuf> { unsafe { known_folder(&knownfolders::FOLDERID_Downloads) } }
 pub fn font_dir()       -> Option<PathBuf> { None }
-pub fn picture_dir()    -> Option<PathBuf> { Some(unsafe { known_folder(&knownfolders::FOLDERID_Pictures) }) }
-pub fn public_dir()     -> Option<PathBuf> { Some(unsafe { known_folder(&knownfolders::FOLDERID_Public) }) }
-pub fn template_dir()   -> Option<PathBuf> { Some(unsafe { known_folder(&knownfolders::FOLDERID_Templates) }) }
+pub fn picture_dir()    -> Option<PathBuf> { unsafe { known_folder(&knownfolders::FOLDERID_Pictures) } }
+pub fn public_dir()     -> Option<PathBuf> { unsafe { known_folder(&knownfolders::FOLDERID_Public) } }
+pub fn template_dir()   -> Option<PathBuf> { unsafe { known_folder(&knownfolders::FOLDERID_Templates) } }
     // see https://github.com/soc/directories-rs/issues/18
     // let trash_dir      = unsafe { known_folder(&knownfolders::FOLDERID_RecycleBinFolder) };
-pub fn video_dir()      -> Option<PathBuf> { Some(unsafe { known_folder(&knownfolders::FOLDERID_Videos) }) }
+pub fn video_dir()      -> Option<PathBuf> { unsafe { known_folder(&knownfolders::FOLDERID_Videos) } }
 
-unsafe fn known_folder(folder_id: shtypes::REFKNOWNFOLDERID) -> PathBuf {
+unsafe fn known_folder(folder_id: shtypes::REFKNOWNFOLDERID) -> Option<PathBuf> {
     let mut path_ptr: winnt::PWSTR = std::ptr::null_mut();
-    let _result = shlobj::SHGetKnownFolderPath(folder_id, 0, std::ptr::null_mut(), &mut path_ptr);
-    let len = length_of_u16_string(path_ptr);
-    let path = std::slice::from_raw_parts(path_ptr, len);
-    let ostr: std::ffi::OsString = std::os::windows::ffi::OsStringExt::from_wide(path);
-    combaseapi::CoTaskMemFree(path_ptr as *mut winapi::ctypes::c_void);
-    PathBuf::from(ostr)
+    let result = shlobj::SHGetKnownFolderPath(folder_id, 0, std::ptr::null_mut(), &mut path_ptr);
+    if result == 0 { // 0 == S_OK
+        let len = length_of_u16_string(path_ptr);
+        let path = std::slice::from_raw_parts(path_ptr, len);
+        let ostr: std::ffi::OsString = std::os::windows::ffi::OsStringExt::from_wide(path);
+        combaseapi::CoTaskMemFree(path_ptr as *mut winapi::ctypes::c_void);
+        Some(PathBuf::from(ostr))
+    } else {
+        None
+    }
 }
 
 unsafe fn length_of_u16_string(ptr: *mut u16) -> usize {
