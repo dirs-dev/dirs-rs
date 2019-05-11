169a95313244f28086e01b345396c8154d78a648
Wang Xuerui <git@xen0n.name>
2019-05-11 18:54:11 +0800

    Use the cfg-if crate for platform support conditionals

--- src/lib.rs
+++ src/lib.rs
@@ -13,18 +13,29 @@
 
 #![deny(missing_docs)]
 
+#[macro_use]
+extern crate cfg_if;
+
 use std::path::PathBuf;
 
-#[cfg(target_os = "windows")]                                                     mod win;
-#[cfg(target_os = "macos")]                                                       mod mac;
-#[cfg(target_os = "redox")]                                                       mod redox;
-#[cfg(not(any(target_os = "windows", target_os = "macos", target_os = "redox")))] mod lin;
-#[cfg(unix)]                                                                      mod unix;
+#[cfg(unix)]
+mod unix;
 
-#[cfg(target_os = "windows")]                                                     use win as sys;
-#[cfg(target_os = "macos")]                                                       use mac as sys;
-#[cfg(target_os = "redox")]                                                       use redox as sys;
-#[cfg(not(any(target_os = "windows", target_os = "macos", target_os = "redox")))] use lin as sys;
+cfg_if! {
+    if #[cfg(target_os = "windows")] {
+        mod win;
+        use win as sys;
+    } else if #[cfg(target_os = "macos")] {
+        mod mac;
+        use mac as sys;
+    } else if #[cfg(target_os = "redox")] {
+        mod redox;
+        use redox as sys;
+    } else {
+        mod lin;
+        use lin as sys;
+    }
+}
 
 /// Returns the path to the user's home directory.
 ///
